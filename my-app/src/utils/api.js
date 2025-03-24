9d1614361d5b8e77adb266a863e35f6b3778bc99
Maryam Tahir <maryamtahir061@gmail.com>
2025-03-24 05:32:20 +0500

    api change

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://107.21.157.243:5000',
+  baseURL: 'http://107.21.157.243:5000/api',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
