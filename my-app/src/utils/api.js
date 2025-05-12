730f34d5c676d3917967f22383f63bbeb0e6b98c
Maryam Tahir <maryamtahir061@gmail.com>
2025-05-12 06:01:12 +0500

    hello

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL:  'http://backend:5000/' || 'http://localhost:5000/',
+  baseURL:  'http://54.159.57.46:5000/',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
